cdcacb8dfa8db733421a30ffc4a65ab53fdc42a3
jacquesPro <jacquesmajyambere79@gmail.com>
2024-11-19 02:25:14 -0500

    the file index added successfully

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>student management system</title>
+</head>
+<body>
+    <center>
+    <form action="" method="POST">
+        <fieldset>
+    <center><h1>login form </h1></center>
+ <label for="">Username:</label><br>
+ <input type="text" name="username" checked><br><br>
+<label for="">Password:</label><br>
+<input type="password" name="password" checked><br><br>
+<input type="submit" name="submit" value="Login">
+
+</fieldset> </form>  
+</center> 
+</body>
+</html>
